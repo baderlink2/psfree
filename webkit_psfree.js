062dd6b7598ca57c8bd03a24dc8aad899b099f64
baderlink2 <bouazzaelharyzy@gmail.com>
2025-04-19 15:36:04 +0200

    Add files via upload

--- webkit_psfree.js
+++ webkit_psfree.js
@@ -679,23 +679,23 @@ async function get_ready() {
 
 async function run_psfree() {
     StartTimer();
-    progress.innerHTML="[+] Webkit exploit (PSFree) (Step 0 - Readying)";
+    progress.innerHTML="[+] Webkit exploit (PSFree) <font color=#fc0404> (Step 0 - Readying)";
     await get_ready();
 
-    progress.innerHTML="[+] Webkit exploit (PSFree) (Step 1 - UAF)";
+    progress.innerHTML="[+] Webkit exploit (PSFree) <font color=#fc0404> (Step 1 - UAF)";
     await use_after_free(pop, s1);
 
     /* we trigger the leak first because it is more likely to work than
        if it were to happen during the second ssv smashing on the ps4 */
 
-    progress.innerHTML="[+] Webkit exploit (PSFree) (Step 2 - Double free)";
+    progress.innerHTML="[+] Webkit exploit (PSFree) <font color=#fc0404>(Step 2 - Double free)";
     /* keeps setup_ar()'s total sleep even lower
        also helps the garbage collector scheduling for 9.xx */
 
     await sleep(0);
     await double_free(s1);
 
-    progress.innerHTML="[+] Webkit exploit (PSFree) (Step 2 - Triple free)";
+    progress.innerHTML="[+] Webkit exploit (PSFree) <font color=#fc0404> (Step 2 - Triple free)";
     await triple_free(s1, jsview, view_leak_arr, view_leak);
 
     // clear_log();
@@ -756,7 +756,7 @@ async function run_psfree() {
         }
     };
     EndTimer();
-    progress.innerHTML="[+] Webkit exploit (PSFree) (Finished)";
+    progress.innerHTML="[+] Webkit exploit (PSFree) <font color=#2afa0a> (Finished)";
     window.p = prim;
     run_hax();
 }
